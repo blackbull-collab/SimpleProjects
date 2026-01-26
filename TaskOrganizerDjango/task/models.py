c6e3388dcca784c8ea52fd5f1fbd7bb238fe9ec2
vinoth <itsvinoth.x@gmail.com>
2026-01-27 00:02:23 +0530

    Added models in project

--- TaskOrganizerDjango/task/models.py
+++ TaskOrganizerDjango/task/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed')
+    ]
+    
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+
+    duedate=models.DateField()
+
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
--- TaskOrganizerDjango/task/urls.py
+++ TaskOrganizerDjango/task/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name="task"
+
 urlpatterns = [
     path("task_list",views.task_list,name="tasklist"),
     path("task_detail",views.task_detail,name="taskdetail"),
