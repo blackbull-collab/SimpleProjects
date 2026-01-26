88f2dbd8a7cb3970de0ab1950e940afee06ed8a3
vinoth <itsvinoth.x@gmail.com>
2026-01-26 22:57:54 +0530

    Included urls

--- TaskOrganizerDjango/task/views.py
+++ TaskOrganizerDjango/task/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def task_list(request):
     return render(request,"tasks/task_list.html")
+
+def task_detail(request):
+    return render(request,"tasks/task_detail.html")
+
+def create_task(request):
+    return render(request,"tasks/create_tasks.html")
+
+def edit_task(request):
+    return render(request,"tasks/edit_tasks.html")
